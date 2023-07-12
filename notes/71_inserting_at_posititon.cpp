ce217ee6d281485474eec191cf4532a12b59f229
Ritik <ritik221jaggi@gmail.com>
2023-07-12 14:18:30 +0530

    done

--- notes/71_inserting_at_posititon.cpp
+++ notes/71_inserting_at_posititon.cpp
@@ -28,6 +28,65 @@ Node *insertInEnd(Node * head, int d)
     return head;
 
 }
+Node * insertAtPos(Node * &head,int d,int pos)
+{
+    Node * temp = new Node(d);
+
+    // corner case
+    if(pos==1)
+    {
+        temp->next = head;
+        return temp;
+    }
+    Node * curr = head;
+    for(int i = 1; i <= pos-2 && curr!=NULL; i++)
+    {
+        curr = curr->next;
+    }
+
+    if(curr==NULL)   // when input is beyond size 
+    return head;
+
+
+    temp->next = curr->next;
+    curr->next = temp;
+    return head;
+}
+Node * deleteFirstElement(Node * &head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    else{
+        Node * temp = head->next;
+        delete head;
+        return temp;
+    }
+}
+Node * deleteLastElement(Node * &head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    if(head->next==NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    else
+    {
+        Node * curr = head;
+        while(curr->next->next!=NULL)
+        {
+            curr = curr->next;
+        }
+        delete curr->next;
+        curr->next = NULL;
+        return head;
+    }
+}
 void printll(Node * head)
 {
     if(head==NULL)
@@ -43,6 +102,10 @@ int main()
     head = insertInEnd(head,10);
     head = insertInEnd(head,20);
     head = insertInEnd(head,30);
+    // printll(head);
+    // head = insertAtPos(head,15,4);
+    // head = deleteFirstElement(head);
+    head = deleteLastElement(head);
     printll(head);
     return 0;
 }
